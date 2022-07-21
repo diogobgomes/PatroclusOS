d8fd8f2cd5b78a7ad53d05931b00b9b657991fe6
Diogo Gomes <dbarrosgomes@gmail.com>
2022-07-22 00:48:52 +0100

    Now able to print "Hello world" using int 10h

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 tools=/home/dbarrosgomes/Documents/Programming/OSDev/tools/cross/bin/i686-elf
 
 $tools-as -o boot.o boot.S
+#Boot sector loaded by BIOS at 0x0700
 $tools-ld --oformat binary -o boot.bin -Ttext 0x7C00 boot.o
 
 qemu-system-i386 -hda boot.bin
